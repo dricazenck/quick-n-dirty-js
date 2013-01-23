f97771690109ec25f9ddabf428a3f5c27f284aa9
Rafael <rafael@rafael-Dell-System-XPS-L502X.(none)>
2013-01-22 23:23:12 -0200

    Working

--- qnd-02/challenge-1.js
+++ qnd-02/challenge-1.js
@@ -1,15 +1,42 @@
-var potencia;
+// var potencia;
 
-var quadrado = potencia(2);
+// var quadrado = potencia(2);
 
-console.log(quadrado(2)); // 4
-console.log(quadrado(3)); // 9
-console.log(quadrado(4)); // 16
+// console.log(quadrado(2)); // 4
+// console.log(quadrado(3)); // 9
+// console.log(quadrado(4)); // 16
 
-var cubo = potencia(3);
+// var cubo = potencia(3);
 
-console.log(cubo(2)); // 8
-console.log(cubo(3)); // 27
-console.log(cubo(4)); // 64
+// console.log(cubo(2)); // 8
+// console.log(cubo(3)); // 27
+// console.log(cubo(4)); // 64
 
 // implementar a função potência usando closure.
+
+var Pow = function(exponent){
+
+	this.exponent = exponent;
+
+
+	return {
+		potecia : function(base) {
+			return Math.pow(base,exponent);
+		} 
+
+	}
+
+};
+
+var quadrado = Pow(2);
+
+console.log(quadrado.potecia(2));
+console.log(quadrado.potecia(3));
+console.log(quadrado.potecia(4));
+
+var cubo = Pow(3);
+
+console.log(cubo.potecia(2));
+console.log(cubo.potecia(3));
+console.log(cubo.potecia(4));
+
--- qnd-02/challenge-2.js
+++ qnd-02/challenge-2.js
@@ -1,7 +1,21 @@
 // implementar usando module-pattern e as funções do challenge-1, um módulo que
 // contenha a api "quadrado" e "cubo" públicas, valendo!
 
-var module; // implementar essa bosta
+var module = (function(){
+
+	var QUADRADO = 2;
+	var CUBO = 3;
+
+	return {
+		quadrado: function(base) {
+			return Math.pow(base,QUADRADO);
+		},
+		cubo: function(base) {
+			return Math.pow(base,CUBO);
+		}
+
+	};
+}()); // implementar essa bosta
 
 console.log(module.quadrado(2)); // 4
 console.log(module.quadrado(3)); // 9
@@ -10,3 +24,5 @@ console.log(module.quadrado(4)); // 16
 console.log(module.cubo(2)); // 8
 console.log(module.cubo(3)); // 27
 console.log(module.cubo(4)); // 64
+
+
